206b28552805e2ae3714a41652347f54f8bc74b3
Kismat Raj <kismat.vishwakarma@gmail.com>
2023-12-27 17:06:03 +0530

    Data Mutation

--- src/app/api/products/[productId]/route.ts
+++ src/app/api/products/[productId]/route.ts
@@ -8,7 +8,7 @@ type TProps = {
 
 export async function GET(req: Request, { params: { productId } }: TProps) {
   const res = await fetch(`https://dummyjson.com/products/${productId}`);
-  const data: TProduct = await res.json();
-  if (!data) undefined;
-  return NextResponse.json(data);
+  // const data: TProduct = await res.json();
+  if (!res.ok) undefined;
+  return NextResponse.json(res);
 }
---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(req: Request) {
+  const allowedOrigins = [".*"];
+  //   const regex = new RegExp("/api/:.*");
+  //   if (regex.test(req.url)) {
+  const origin = req.headers.get("origin") as string;
+  console.log(origin);
+
+  if (origin && !allowedOrigins.includes(origin))
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "Bad request",
+      headers: { "Content-Type": "text/plain" },
+    });
+
+  console.log(process.env.NODE_ENV);
+  console.log(req.method);
+  console.log(req.url);
+
+  return NextResponse.next();
+}
+// export const config = {
+//   matcher: ["/api/:path*"],
+// };
